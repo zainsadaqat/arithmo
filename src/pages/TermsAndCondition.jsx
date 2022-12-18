7c33a25d86e1b05e45f0faa6faf7d2852ec02afb
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-12-18 21:54:25 +0500

    Create terms and condition page

---
+++ src/pages/TermsAndCondition.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const TermsAndCondition = () => {
+  return (
+    <section className="max-w-6xl m-auto px-4 py-2 mb-16">
+      <h2 className="text-[30px] text-center text-[#0029FF] font-bold md:text-[40px]">
+        Terms and Conditions
+      </h2>
+      <div className="text-center text-[#111] text-[16px] font-medium md:text-left">
+        <p className="my-4">
+          Help protect your website and its users with clear and fair website
+          terms and conditions. These terms and conditions for a website set out
+          key issues such as acceptable use, privacy, cookies, registration and
+          passwords, intellectual property, links to other sites, termination
+          and disclaimers of responsibility. Terms and conditions are used and
+          necessary to protect a website owner from liability of a user relying
+          on the information or the goods provided from the site then suffering
+          a loss.
+        </p>
+        <p className="my-4">
+          Making your own terms and conditions for your website is hard, not
+          impossible, to do. It can take a few hours to few days for a person
+          with no legal background to make. But worry no more; we are here to
+          help you out.
+        </p>
+        <p className="my-4">
+          All you need to do is fill up the blank spaces and then you will
+          receive an email with your personalized terms and conditions.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default TermsAndCondition;
