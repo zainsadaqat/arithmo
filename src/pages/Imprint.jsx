121c31e691e3c1096e7de78b3ee79fee5aa4672b
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-12-18 21:57:24 +0500

    Creat imprint page

---
+++ src/pages/Imprint.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Imprint = () => {
+  return (
+    <section className="max-w-6xl m-auto px-4 py-2 mb-16">
+      <h2 className="text-[30px] text-center text-[#0029FF] font-bold md:text-[40px]">
+        Imprint
+      </h2>
+      <ul className="text-[#111] text-[16px] font-medium">
+        <li>Max Mustermann</li>
+        <li>Lange Straße 20</li>
+        <li>7080 Berlin</li>
+        <li>Germany</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Imprint;
