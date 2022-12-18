41c648957cbb1279ef51fd62aa8f1dd56c3fefbe
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-12-18 21:57:35 +0500

    Create cookie policy page

---
+++ src/pages/CookiePolicy.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const CookiePolicy = () => {
+  return (
+    <section className="max-w-6xl m-auto px-4 py-2 mb-16">
+      <h2 className="text-[30px] text-center text-[#0029FF] font-bold md:text-[40px]">
+        Cookie Policy
+      </h2>
+      <div className="text-[#111] text-[16px] font-medium">
+        <p className="my-4">
+          Cookies are small text files that websites place on the computers and
+          mobile devices of people who visit those websites.
+        </p>
+        <p className="my-4">
+          These text files allow a website to remember your device and how you
+          interacted with the website, which is useful for a number of different
+          purposes.
+        </p>
+        <p className="mt-16">
+          For example, cookies can be used to remember username and password
+          information so that you don’t have to re-enter all of your login
+          information every time you visit a site you frequently log in to.
+        </p>
+        <p className="my-4">
+          Other functions of cookies are to provide custom advertising to users
+          based on searches and personal interests, as well as site performance
+          cookies that enhance website use by remembering things such as custom
+          video streaming or volume settings you have selected while using the
+          website in the past.
+        </p>
+        <p className="my-4">
+          If you use cookies, you should consider having a Cookies Policy. In
+          fact, you may be legally required to have one.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default CookiePolicy;
