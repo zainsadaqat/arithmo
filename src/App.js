bb562b09d605d482547ff6ffc9c9a66c3ea604a3
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-12-18 21:59:42 +0500

    Add routes for the pages

--- src/App.js
+++ src/App.js
@@ -11,14 +11,40 @@ import GetReady from './components/GetReady';
 import GetInTouch from './components/GetInTouch';
 import Footer from './components/Footer';
 import Features from './components/Features';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsAndCondition from './pages/TermsAndCondition';
+import Imprint from './pages/Imprint';
+import CookiePolicy from './pages/CookiePolicy';
+import AboutUs from './pages/AboutUs';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import FAQ from './pages/FAQ';
+import Blog from './pages/Blog';
 
 function App() {
   return (
     <div>
-      <header className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
-        <Navbar />
+      <header className="bg-white sticky top-0 z-50 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+        <div className="relative">
+          <Navbar />
+        </div>
       </header>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-and-condition" element={<TermsAndCondition />} />
+        <Route path="/imprint" element={<Imprint />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/blogs" element={<Blog />} />
+      </Routes>
+
       <main className="min-h-[80vh] pt-16 md:pt-32">
+        {/* <CookiePolicy /> */}
+        {/* <Imprint /> */}
+        {/* <TermsAndCondition /> */}
+        {/* <PrivacyPolicy /> */}
         <Hero />
         <Stats />
       </main>
@@ -30,7 +56,9 @@ function App() {
       <ArithmoWorks />
       <GetReady />
       <Features />
-      <GetInTouch />
+      <div id="contact" className="max-w-5xl mx-auto">
+        <GetInTouch />
+      </div>
       <Footer />
     </div>
   );
