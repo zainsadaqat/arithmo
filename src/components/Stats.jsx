13d63fae27e197863520a2712af33c7a3ab773a0
Zain Sadaqat <zain.sadaqet@gmail.com>
2023-01-02 22:57:01 +0500

    Add border and its properties to stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -12,7 +12,7 @@ const Stats = () => {
         Focus on the important parts of your business
       </h3>
       <div className="flex flex-col justify-center md:flex-row">
-        <div className="flex justify-center items-center border-4 px-4 m-4">
+        <div className="flex justify-center items-center border-[#f2f3fb] border-[6px] rounded-lg px-4 m-4">
           <span className="px-2 text-[14px] font-bold">MIN</span>
           <span className="px-2 font-bold text-[36px] text-[#0029FF]">
             10X*
@@ -22,7 +22,7 @@ const Stats = () => {
             <img src={GraphUp} alt="Graph Up Image" width={80} height={80} />
           </div>
         </div>
-        <div className="flex justify-center items-center border-4 px-4 m-4">
+        <div className="flex justify-center items-center border-[#f2f3fb] border-[6px] rounded-lg px-4 m-4">
           <div className="p-4">
             <img src={GraphDown} alt="Graph Up Image" width={80} height={80} />
           </div>
@@ -33,7 +33,7 @@ const Stats = () => {
           <span className="px-2 text-[14px] font-bold">CHEAPER</span>
         </div>
       </div>
-      <p className="text-[14px] text-center pb-8">
+      <p className="text-[14px] text-center pb-8 uppercase">
         *compared to manual labor work
       </p>
     </section>
